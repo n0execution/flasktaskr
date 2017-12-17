946f26ef8cb5cd6dbd4fdc721cb49b30551c4cee
qwerty432 <dimichkit@ukr.net>
2017-12-17 16:43:46 +0200

    added test_api.py

--- fabfile.py
+++ fabfile.py
@@ -4,45 +4,45 @@ from fabric.contrib.console import confirm
 
 #preparation
 def test():
-	with settings(warn_only=True):
-		result = local("nosetests -v", capture=True)
-	if result.failed and not confirm("Tests failed. Continue?"):
-		abort("Aborted at user request.")
+    with settings(warn_only=True):
+        result = local("nosetests -v", capture=True)
+    if result.failed and not confirm("Tests failed. Continue?"):
+        abort("Aborted at user request.")
 
 def commit():
-	message = input("Enter a git commit message: ")
-	local("git add . && git commit -am '{}'".format(message))
+    message = input("Enter a git commit message: ")
+    local("git add . && git commit -am '{}'".format(message))
 
 def push():
-	local("git push origin master")
+    local("git push origin master")
 
 def prepare():
-	test()
-	commit()
-	push()
+    test()
+    commit()
+    push()
 
 #deploy
 
 def pull():
-	local("git pull origin master")
+    local("git pull origin master")
 
 def heroku():
-	local("git push heroku master")
+    local("git push heroku master")
 
 def heroku_test():
-	local("heroku run nosetests -v")
+    local("heroku run nosetests -v")
 
 #deployement is not needed cause my heroku app is integrated with github
 def deploy():
-	# pull()
-	test()
-	# commit()
-	heroku()
-	heroku_test()
+    # pull()
+    test()
+    # commit()
+    heroku()
+    heroku_test()
 
 
 # rollback
 def rollback():
-	local("heroku rollback")
+    local("heroku rollback")
 
-	
+    
