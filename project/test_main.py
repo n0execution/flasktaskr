dbada17c305dfa146494201c4233570eac278b1d
qwerty432 <dimichkit@ukr.net>
2017-12-24 23:55:10 +0200

    added tests

--- project/test_main.py
+++ project/test_main.py
@@ -37,6 +37,12 @@ class MainTests(unittest.TestCase):
         self.assertIn(b'Sorry. There\'s nothing here.', response.data)
 
 
+    def test_index(self):
+        """ Ensure flask was set up correctly. """
+        response = self.app.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+
 
 if __name__ == "__main__":
     unittest.main()
